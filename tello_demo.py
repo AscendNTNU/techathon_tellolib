2a02c556e2d23017e6b672d8835dae9d6c2a5983
Torjus Bakkene <torjus.bakkene@gmail.com>
2019-10-05 12:03:45 +0200

    Move tellolib to own folder for clarity

--- tello_demo.py
+++ tello_demo.py
@@ -1,13 +1,13 @@
-from dronelib_tello import TelloDrone
-from util import *
+from dronelib.dronelib_tello import TelloDrone
+from dronelib.util import *
 from time import sleep
 
 def main():
     drone = TelloDrone()
     drone.activate()
     drone.takeoff()
-    #image = drone.camera_image()
-    #save_image(image)
+    image = drone.camera_image()
+    save_image(image)
 
     drone.land()
 
